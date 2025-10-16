6b37b0994fe5a0f5dd27fea48297d4f5ae8e8977
Charlotte Tsui <charlottetung@outlook.com>
2025-10-16 19:58:23 -0400

    Remove dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${roboto.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${roboto.variable} antialiased bg-white min-h-screen`}
       >
         {isMounted && (
           <>
